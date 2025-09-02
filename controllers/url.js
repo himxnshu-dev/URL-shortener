5508a11f1395966648d59983cd2a55cea35cbeb8
himanshu <khatri.hk18@gmail.com>
2025-09-02 20:24:41 +0530

    Add url and user controllers

---
+++ controllers/url.js
@@ -0,0 +1,67 @@
+const URL = require("../models/url");
+const {nanoid} = require("nanoid");
+const express = require("express");
+
+const handleGenerateShortURL = async (req, res) => {
+  const {body} = req;
+  const shortID = nanoid(8);
+
+  if (!body.url) {
+    return res.status(400).json({
+      msg: "A valid URL is required!",
+    });
+  }
+
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+
+  return res.status(201).render("home", {
+    id: shortID,
+  });
+};
+
+const handleGetFromShortURL = async (req, res) => {
+  const shortId = req.params.shortId;
+
+  const url = await URL.findOneAndUpdate(
+    {shortId},
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  return res.redirect(url.redirectURL);
+};
+
+const handleShortUrlAnalytics = async (req, res) => {
+  const shortId = req.params.shortId;
+
+  const urlInfo = await URL.findOne({shortId});
+  return res.status(200).json({
+    totalClicks: urlInfo.visitHistory.length,
+    analytics: urlInfo.visitHistory,
+  });
+};
+
+const handleDeleteFromDB = async (req, res) => {
+  const shortId = req.params.shortId;
+
+  await URL.findOneAndDelete({shortId});
+  return res.status(200).json({
+    msg: `url with short ID ${shortId} deleted!`,
+  });
+};
+
+module.exports = {
+  handleGenerateShortURL,
+  handleGetFromShortURL,
+  handleShortUrlAnalytics,
+  handleDeleteFromDB,
+};
---
+++ controllers/user.js
@@ -0,0 +1,15 @@
+const User = require("../models/user");
+
+const handleNewUserSignup = async (req, res) => {
+  const {name, email, password} = req.body;
+
+  await User.create({
+    name,
+    email,
+    password,
+  });
+
+  return res.render("home");
+};
+
+module.exports = {handleNewUserSignup};
